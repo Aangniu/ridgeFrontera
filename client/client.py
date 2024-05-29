c2e78e1e6549fcb82ed7a7e5f220ce5311401693
Zihua Niu <zniu2025@login2.frontera.tacc.utexas.edu>
2024-05-29 11:01:41 -0500

    changed for comparing with real data

--- client/client.py
+++ client/client.py
@@ -2,6 +2,7 @@ import argparse
 import multiprocessing as mp
 import time
 import umbridge
+import numpy as np
 
 if __name__ == "__main__":
         parser = argparse.ArgumentParser()
@@ -20,12 +21,14 @@ if __name__ == "__main__":
                         time.sleep(10)
 
         def eval_um_model(Cd):
-                config = {"meshFile": "Ridgecrest_NewModel1_f200_topo1000_noRef_xml_UBC"}
+                config = {"meshFile": "Ridgecrest_NewModel1_f200_topo1000_noRef_xml_UBC", "order": 4}
                 return model([[Cd]], config)
 
         start_time = time.time()
-        with mp.Pool(10) as p:
-                result = p.map(eval_um_model, [1,0.5,10])
+        inp_np = np.linspace(0.5,3.5,31)
+        inp_list = inp_np.tolist()
+        with mp.Pool(1) as p:
+                result = p.map(eval_um_model, [0.8])
                 print(result)
         end_time = time.time()
         print(end_time - start_time)
--- server/server.py
+++ server/server.py
@@ -3,7 +3,7 @@ import jinja2
 
 import time
 
-# import misfits_kath
+import misfits_kath as msf
 import scipy.interpolate as sp_int
 
 import numpy as np
@@ -12,6 +12,7 @@ import subprocess
 import sys
 import umbridge
 
+import pandas as pd
 
 def gpu_available():
     # Hard coded for now, until I find a better way to automatically check,
@@ -20,12 +21,15 @@ def gpu_available():
     return False
 
 
-def seissol_command(run_id="", ranks=4):
+def seissol_command(run_id="", ranks=4, order=4):
     if gpu_available():
         return f"not supported yet for nonlinear seissol"
     else:
-        return f"mpiexec.hydra -n {ranks} -machinefile $HQ_NODE_FILE apptainer run ../seissol.sif SeisSol_Release_dskx_4_viscoelastic2 {run_id}/parameters_40s.par"
-
+        #return f"mpiexec.hydra -n {ranks} -machinefile $HQ_NODE_FILE apptainer run ../seissol_auto.sif SeisSol_Release_dskx_4_viscoelastic2 {run_id}/parameters_40s.par"
+        if order==4:
+            return f"mpiexec.hydra -n {ranks} -machinefile $MACHINE_FILE /work2/09840/zniu2025/frontera/seisRidge/SeisSol/build_app/SeisSol_Release_dskx_4_viscoelastic2 {run_id}/parameters_40s.par"
+        elif order==3:
+            return f"mpiexec.hydra -n {ranks} -machinefile $MACHINE_FILE /work2/09840/zniu2025/frontera/seisRidge/SeisSol/build_O3app/SeisSol_Release_dskx_3_viscoelastic2 {run_id}/parameters_40s.par"
 
 class SeisSol(umbridge.Model):
     def __init__(self, ranks):
@@ -38,10 +42,11 @@ class SeisSol(umbridge.Model):
         return [1]
 
     def get_output_sizes(self, config):
-        return [100]
+        return [1]
 
     def prepare_filesystem(self, parameters, config):
-        param_conf_string = str((parameters, config)).encode("utf-8")
+        submission_time = time.ctime(time.time())
+        param_conf_string = str((parameters, config,submission_time)).encode("utf-8")
         print(param_conf_string)
 
         m = hashlib.md5()
@@ -80,21 +85,24 @@ class SeisSol(umbridge.Model):
         my_env["TACC_AFFINITY_ENABLED"] = "1"
         my_env["OMP_NUM_THREADS"] = "54"
         my_env["OMP_PLACES"] = "cores(54)"
+        my_env["I_MPI_SHM_HEAP_VSIZE"] = "65536"
         return my_env
 
     def __call__(self, parameters, config):
         if not config["meshFile"]:
             config["meshFile"] = "Ridgecrest_NewModel1_f200_topo1000_noRef_xml_UBC"
+        if not config["order"]:
+            config["order"] = 4
         run_id = self.prepare_filesystem(parameters, config)
 
         # time.sleep(self._sleep_time)
 
-        command = seissol_command(run_id, self.ranks)
+        command = seissol_command(run_id, self.ranks, config["order"])
         print(command)
         my_env = self.prepare_env()
         sys.stdout.flush()
         print("reached the part for launching seissol....")
-        subprocess.run("cat $HQ_NODE_FILE", shell=True)
+        subprocess.run("cat $MACHINE_FILE", shell=True)
         result = subprocess.run(command, shell=True, env=my_env)
         print("passed the part for launching seissol....")
         result.check_returncode()
@@ -111,7 +119,7 @@ class SeisSol(umbridge.Model):
         moment_d = data["measurement"][idMoment]
         mrate_data = np.gradient(moment_d,dt)
 
-        times_interp = np.linspace(0.001,39.999,80)
+        times_interp = np.linspace(0.001,19.999,80)
 
         interpolator = sp_int.interp1d(time.values, mrate_data)
 
@@ -119,14 +127,73 @@ class SeisSol(umbridge.Model):
 
         ## Reference
 
-        ref = np.load(os.path.join(run_id, "MT1_Moment_rate_array.npy"))
+        ref = np.load(os.path.join("ref", "MT1_Moment_rate_array.npy"))
 
         interpolatorRef = sp_int.interp1d(ref[:,0], ref[:,1])
 
-        mR_interpolated = interpolator(times_interp)
+        mR_interpolated = interpolatorRef(times_interp)
+
+        #print(mR_interpolated,mD_interpolated)
 
         print(mD_interpolated - mR_interpolated)
-        return mD_interpolated - mR_interpolated
+
+        m_diff = mD_interpolated - mR_interpolated
+        
+        #=================GPS====================
+        prefix = 'ridgecrest'
+        directory = run_id
+
+        ref_GPS = np.load(os.path.join("ref", "dataGPSForCompare.npy"))
+
+        rec_comps = ['v1','v2','v3']
+        
+        #=================covariance=========
+        cov_diag = 1e-3*np.ones(30)
+        cov_diag[0] = 6e-3
+        cov_diag[2] = 3e-3
+        cov_diag[3] = 7e-3
+        cov_diag[10] = 3e-3
+        cov_diag[13] = 3e-3
+        cov_diag[20] = 2e-3
+        cov_diag[22] = 3e-3
+        cov_diag[23] = 3e-3
+        cov_diag[24] = 2e-3
+        cov_diag[25] = 2e-3
+        cov_diag[26] = 5e-3
+        cov_diag[27] = 4e-3
+        #==================================
+        nRec = 10
+
+        diffNorms = []
+        log_likelihood = 0.0
+        y_outSquared = 0.0
+        sig_y = 1.0/np.sqrt(2.0)
+        for i_c in range(3):
+            for i_s in range(nRec):
+                sim_result = msf.read_receiver(
+                    msf.find_receiver(directory,prefix,i_s+1))
+                dt = sim_result['Time'][1] - sim_result['Time'][0]
+                displace = np.cumsum(sim_result[ rec_comps[i_c] ])*dt
+
+                dispRef = ref_GPS[i_c][i_s][0:21]
+
+                # Interpoloate simulation results to compare with Ref
+                # print('Sim time series: ', sim_result['Time'])
+                times_interp = np.linspace(0.001,19.99,21)
+                interpolatorGPS = sp_int.interp1d(sim_result['Time'], displace)
+
+                dispSim_interpolated = interpolatorGPS(times_interp)
+
+                diffGPS = dispSim_interpolated - dispRef
+                diffNorms.append(np.linalg.norm(diffGPS)**2)
+                y_outSquared += np.linalg.norm(diffGPS)**2/cov_diag[i_c*nRec+i_s]/3/nRec
+                #log_likelihood += np.linalg.norm(diffGPS)**2/cov_diag[i_c*nRec+i_s]
+                print(y_outSquared)
+        print(diffNorms)
+        log_likelihood = 0.5*y_outSquared/sig_y**2
+        return [[-log_likelihood]]
+        #return [diffNorms]
+        #return [m_diff.tolist()]
 
     def supports_evaluate(self):
         return True
